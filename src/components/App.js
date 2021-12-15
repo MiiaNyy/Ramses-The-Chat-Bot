fa21926e4fe51b3dbb3bc1fdbac6ccf16daa56aa
Miia Nyy <66413480+MiiaNyy@users.noreply.github.com>
2021-12-15 17:46:19 +0100

    added more text sections and modified styles on bigger screens

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,13 @@ import React from 'react'
 import Navigation from "./Navigation";
 
 import teddyBearImg from "../assets/jerry-wang-cCjlhaRxNsM-unsplash.jpg";
+import officeWorkers from "../assets/brooke-cagle-g1Kr4Ozfoac-unsplash.jpg";
+
 import ImageCarousel from "./ImageCarousel";
 import Footer from "./Footer";
+
+/*Photo by <a href="https://unsplash.com/@brookecagle?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Brooke Cagle</a> on <a href="https://unsplash.com/s/photos/office?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+ */
 
 function App () {
     return (
@@ -11,43 +16,107 @@ function App () {
             <Navigation/>
             <main>
                 <div>
-                    <img src={ teddyBearImg } alt="multiple stuffed animals on the counter"/>
-                    <small>Photo by <a
-                        href="https://unsplash.com/@jerry_318?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-                        Jerry Wang</a> on <a
-                        href="https://unsplash.com/s/photos/teddy-bear?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-                    </small>
-                </div>
-                <section>
-                    <h2 className="text-center">About our company</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium deleniti dolor fugiat!
-                        Atque harum ipsam ipsum laboriosam officia perferendis similique. Ab delectus est, facere harum
-                        numquam quam sit voluptatibus. Distinctio?</p>
-                </section>
-                
-                <hr/>
-                
-                <section>
-                    <div className="section__accent">
-                        <h2 className="text-center">To our employees</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium deleniti dolor fugiat!
-                            Atque harum ipsam ipsum laboriosam officia perferendis similique. Ab delectus est, facere harum
-                            numquam quam sit voluptatibus. Distinctio?</p>
-                        <a href="" className="btn">Check our inventory</a>
+                    <div>
+                        <img className={ `top-img` } src={ teddyBearImg }
+                             alt="multiple stuffed animals on the counter"/>
+                        <small>Photo by <a
+                            href="https://unsplash.com/@jerry_318?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+                            Jerry Wang</a> on <a
+                            href="https://unsplash.com/s/photos/teddy-bear?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                        </small>
                     </div>
-                </section>
+                    
+                    <section>
+                        <h2 className="text-center">About our company</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium deleniti dolor fugiat!
+                            Atque harum ipsam ipsum laboriosam officia perferendis similique. Ab delectus est, facere
+                            harum
+                            numquam quam sit voluptatibus. Distinctio?</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam cum doloremque molestiae
+                            nam provident quis totam. A consequatur corporis ducimus eaque ipsum nesciunt nostrum quod
+                            sed totam? Aut saepe, velit?</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem doloribus earum eum ipsa
+                            itaque nulla quas rerum unde vel vero? Ducimus molestiae nobis totam voluptate. Earum odit
+                            repellat reprehenderit. Aspernatur!</p>
+                    </section>
+                </div>
                 
-                
-                <ImageCarousel/>
+                <div className="flex-container">
+                    <div>
+                        <img className={ `narrow-img` } src={ officeWorkers }
+                             alt="Three people sitting in front of desk with laptops and laughing"/>
+                        Photo by <a
+                        href="https://unsplash.com/@brookecagle?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+                        Brooke Cagle</a>
+                        on <a
+                        href="https://unsplash.com/s/photos/office?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+                        Unsplash</a>
+                    </div>
+                    
+                    <section className="section__accent">
+                        <div>
+                            <h2 className="text-center">To our employees</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium deleniti dolor
+                                fugiat! Atque harum ipsam ipsum laboriosam officia perferendis similique. Ab delectus
+                                est,
+                                facere harumnumquam quam sit voluptatibus. Distinctio?</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa delectus dolorem ex illo
+                                incidunt itaque laborum modi necessitatibus, nisi nulla perferendis porro quaerat quidem
+                                rerum sapiente sequi soluta temporibus ut.</p>
+                            <a href="" className="btn">Check our inventory</a>
+                        </div>
+                    </section>
+                </div>
                 
                 <section>
                     <h2 className="text-center">What we sell?</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium deleniti dolor fugiat!
                         Atque harum ipsam ipsum laboriosam officia perferendis similique. Ab delectus est, facere harum
                         numquam quam sit voluptatibus. Distinctio?</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad atque deleniti, deserunt dolor
+                        doloribus, ducimus esse illo natus nobis nostrum omnis placeat porro possimus praesentium quae,
+                        quaerat quos reprehenderit rerum!
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias asperiores at commodi
+                        consequatur in iste, iure libero magnam minima neque, provident quas quibusdam, quisquam ratione
+                        suscipit! Assumenda doloremque nulla veniam!
+                    </p>
                     <a href="" className="btn">Our online store</a>
                 </section>
-            
+                
+                
+                <div className="section__containers">
+                    <div className="section__containers-item">
+                        <h4>Lorem ipsum dolor</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                            Accusantium deleniti dolor fugiat!
+                            Atque harum ipsam ipsum laboriosam officia perferendis similique. Ab delectus est, facere
+                            harum
+                            numquam quam sit voluptatibus. Distinctio?
+                        </p>
+                    </div>
+                    <div className="section__containers-item">
+                        <h4>Lorem ipsum dolor</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad
+                            atque deleniti, deserunt dolor
+                            doloribus, ducimus esse illo natus nobis nostrum omnis placeat porro possimus praesentium
+                            quae,
+                            quaerat quos reprehenderit rerum!
+                        </p>
+                    </div>
+                    <div className="section__containers-item">
+                        <h4>Lorem ipsum dolor</h4>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias asperiores at commodi
+                            consequatur in iste, iure libero magnam minima neque, provident quas quibusdam, quisquam
+                            ratione
+                            suscipit! Assumenda doloremque nulla veniam!
+                        </p>
+                    </div>
+                </div>
+                
+                <ImageCarousel/>
             </main>
             
             <Footer/>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,33 @@
 import React from 'react';
 
+function openNavigation() {
+    document.querySelectorAll('.nav__list').forEach(item => {
+        item.classList.toggle('visible');
+    })
+    document.querySelector('.nav-icon__bars').style.display = 'none';
+    document.querySelector('.nav-icon__cross').style.display = 'block';
+}
+
+function closeNavigation() {
+    document.querySelectorAll('.nav__list').forEach(item => {
+        item.classList.toggle('visible');
+    })
+    document.querySelector('.nav-icon__bars').style.display = 'block';
+    document.querySelector('.nav-icon__cross').style.display = 'none';
+}
+
 function Navigation () {
     return (
         <nav className="navbar navbar-dark bg-dark">
-            <div className="mx-auto text-center navbar-flex-item">
-                <a className="navbar-brand">Rafael's Stuffed Animals</a>
-                <ul className="navbar nav">
+            <div className="navbar">
+                <div className={`d-flex justify-content-between align-items-center`}>
+                    <a className="navbar-brand">Rafael's Stuffed Animals</a>
+                    <a><i onClick={ () => openNavigation()} className="fas fa-bars nav-icon__bars"/></a>
+                    <a className={`nav-icon__cross`}><i onClick={() => closeNavigation()} className="fas fa-times "/></a>
+                </div>
+                
+                
+                <ul className="navbar nav text-center nav__list">
                     <li className="nav-item active">
                         <a className="nav-link" href="/">Home</a>
                     </li>
@@ -17,7 +39,7 @@ function Navigation () {
                     </li>
                 </ul>
             </div>
-            <ul className="navbar nav mx-auto text-center">
+            <ul className="navbar nav text-center pb-4 nav__list">
                 <li className="nav-item">
                     <a className="nav-link" href="/sign-up">Sign up</a>
                 </li>
@@ -30,3 +52,30 @@ function Navigation () {
 }
 
 export default Navigation;
+
+
+/*<nav className="navbar navbar-dark bg-dark">
+ <div className="mx-auto navbar-flex-item ">
+ <a className="navbar-brand">Rafael's Stuffed Animals</a>
+ 
+ <ul className="navbar nav nav__box">
+ <li className="nav-item active">
+ <a className="nav-link" href="/">Home</a>
+ </li>
+ <li className="nav-item">
+ <a className="nav-link" href="/category">Categories</a>
+ </li>
+ <li className="nav-item">
+ <a className="nav-link" href="/item/create">Create new</a>
+ </li>
+ </ul>
+ </div>
+ <ul className="navbar nav mx-auto text-center nav__box">
+ <li className="nav-item">
+ <a className="nav-link" href="/sign-up">Sign up</a>
+ </li>
+ <li className="nav-item active">
+ <a className="nav-btn" href="/login">Log in</a>
+ </li>
+ </ul>
+ </nav>*/
