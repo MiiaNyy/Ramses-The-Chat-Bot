794cdb95a6b507e5663e6387d3d8140b2a6e5b35
Miia Nyy <66413480+MiiaNyy@users.noreply.github.com>
2021-12-20 15:50:51 +0100

    edited footer and main content styeles more

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,15 @@ import Navigation from "./Navigation";
 
 import teddyBearImg from "../assets/jerry-wang-cCjlhaRxNsM-unsplash.jpg";
 import officeWorkers from "../assets/brooke-cagle-g1Kr4Ozfoac-unsplash.jpg";
+import nursery from "../assets/omurden-cengiz--G2iJF_aUws-unsplash.jpg";
 
 import ImageCarousel from "./ImageCarousel";
 import Footer from "./Footer";
 
-/*Photo by <a href="https://unsplash.com/@brookecagle?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Brooke Cagle</a> on <a href="https://unsplash.com/s/photos/office?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
+/*Photo by <a href="https://unsplash.com/@omurden?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Ömürden Cengiz</a> on <a href="https://unsplash.com/s/photos/nursery?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+ 
+ Photo by <a href="https://unsplash.com/@brookecagle?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Brooke Cagle</a> on <a href="https://unsplash.com/s/photos/office?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
  */
 
 function App () {
@@ -16,17 +20,17 @@ function App () {
             <Navigation/>
             <main>
                 <div>
-                    <div>
+                    <div className="position-relative">
                         <img className={ `top-img` } src={ teddyBearImg }
                              alt="multiple stuffed animals on the counter"/>
-                        <small>Photo by <a
+                        <p className="img-credits">Photo by <a
                             href="https://unsplash.com/@jerry_318?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
                             Jerry Wang</a> on <a
                             href="https://unsplash.com/s/photos/teddy-bear?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-                        </small>
+                        </p>
                     </div>
                     
-                    <section>
+                    <section className="container-txt">
                         <h2 className="text-center">About our company</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium deleniti dolor fugiat!
                             Atque harum ipsam ipsum laboriosam officia perferendis similique. Ab delectus est, facere
@@ -40,53 +44,77 @@ function App () {
                             repellat reprehenderit. Aspernatur!</p>
                     </section>
                 </div>
-                
-                <div className="flex-container">
-                    <div>
-                        <img className={ `narrow-img` } src={ officeWorkers }
-                             alt="Three people sitting in front of desk with laptops and laughing"/>
-                        Photo by <a
-                        href="https://unsplash.com/@brookecagle?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-                        Brooke Cagle</a>
-                        on <a
-                        href="https://unsplash.com/s/photos/office?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-                        Unsplash</a>
-                    </div>
-                    
-                    <section className="section__accent">
-                        <div>
-                            <h2 className="text-center">To our employees</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium deleniti dolor
-                                fugiat! Atque harum ipsam ipsum laboriosam officia perferendis similique. Ab delectus
-                                est,
-                                facere harumnumquam quam sit voluptatibus. Distinctio?</p>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa delectus dolorem ex illo
-                                incidunt itaque laborum modi necessitatibus, nisi nulla perferendis porro quaerat quidem
-                                rerum sapiente sequi soluta temporibus ut.</p>
-                            <a href="" className="btn">Check our inventory</a>
+                <div className="bg-dark">
+                    <div className=" container-fluid flex-container">
+                        <div className="flex-container__section">
+                            <img src={ officeWorkers }
+                                 alt="Three people sitting in front of desk with laptops and laughing"/>
+                            <p className="img-credits light-text">
+                                Photo by <a
+                                href="https://unsplash.com/@brookecagle?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+                                Brooke Cagle </a>
+                                on <a
+                                href="https://unsplash.com/s/photos/office?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+                                Unsplash </a>
+                            </p>
                         </div>
-                    </section>
+                        
+                        <section className="flex-container__txt section__accent">
+                            <div className="container-txt">
+                                <h2 className="text-center">To our employees</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium deleniti dolor
+                                    fugiat! Atque harum ipsam ipsum laboriosam officia perferendis similique. Ab
+                                    delectus
+                                    est,
+                                    facere harumnumquam quam sit voluptatibus. Distinctio?</p>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa delectus dolorem ex
+                                    illo
+                                    incidunt itaque laborum modi necessitatibus, nisi nulla perferendis porro quaerat
+                                    quidem
+                                    rerum sapiente sequi soluta temporibus ut.</p>
+                                <div className="text-center mb-4">
+                                    <a href="" className="btn">Check our inventory</a>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
                 </div>
                 
-                <section>
-                    <h2 className="text-center">What we sell?</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium deleniti dolor fugiat!
-                        Atque harum ipsam ipsum laboriosam officia perferendis similique. Ab delectus est, facere harum
-                        numquam quam sit voluptatibus. Distinctio?</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad atque deleniti, deserunt dolor
-                        doloribus, ducimus esse illo natus nobis nostrum omnis placeat porro possimus praesentium quae,
-                        quaerat quos reprehenderit rerum!
-                    </p>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias asperiores at commodi
-                        consequatur in iste, iure libero magnam minima neque, provident quas quibusdam, quisquam ratione
-                        suscipit! Assumenda doloremque nulla veniam!
-                    </p>
-                    <a href="" className="btn">Our online store</a>
-                </section>
                 
-                
-                <div className="section__containers">
+                <div>
+                    <div className="container-fluid flex-container">
+                        <section className="flex-container__txt container-txt">
+                            <h2 className="text-center">What we sell?</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium deleniti dolor
+                                fugiat!
+                                Atque harum ipsam ipsum laboriosam officia perferendis similique. Ab delectus est,
+                                facere
+                                harum
+                                numquam quam sit voluptatibus. Distinctio?</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad atque deleniti, deserunt
+                                dolor
+                                doloribus, ducimus esse illo natus nobis nostrum omnis placeat porro possimus
+                                praesentium
+                                quae,
+                                quaerat quos reprehenderit rerum!
+                            </p>
+                            <div className="text-center">
+                                <a href="" className="btn">Check our inventory</a>
+                            </div>
+                        </section>
+                        
+                        <div className="flex-container__section">
+                            <img src={ nursery } alt="Three people sitting in front of desk with laptops and laughing"/>
+                            <p className="img-credits">
+                                Photo by <a
+                                href="https://unsplash.com/@omurden?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Ömürden
+                                Cengiz</a> on <a
+                                href="https://unsplash.com/s/photos/nursery?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="container-fluid section__containers">
                     <div className="section__containers-item">
                         <h4>Lorem ipsum dolor</h4>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -115,12 +143,9 @@ function App () {
                         </p>
                     </div>
                 </div>
-                
                 <ImageCarousel/>
             </main>
-            
             <Footer/>
-        
         </div>
     
     )
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,51 +3,52 @@ import React from 'react';
 function Footer () {
     return (
         <footer className="bg-dark">
-            <p className="footer__title">Rafael's Stuffed Animal</p>
-            
-            <div className="footer__item">
-                <strong>For Employee</strong>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid blanditiis culpa dolore ea
-                    eligendi, enim esse est et laboriosam, laborum magnam, numquam sunt suscipit unde voluptates. Ea
-                    quia repellat tempora!</p>
-                <a href="" className="btn">Go to Inventory</a>
-            </div>
-            
-            <div className="footer__item">
-                
-                <div className="footer__item-address">
-                    <p className="footer__item-title"><strong>Where to find us</strong> </p>
-                    <p>9 King Street</p>
-                    <p>London</p>
-                    <p>SE86 1NZ</p>
-                    <p>England</p>
-                    <p><strong>email:</strong> lorem@rsa.com </p>
-                    <p><strong>phone:</strong> (123) 123 123</p>
+            <p className="footer__title">RSA - Rafael's Stuffed Animals</p>
+            <div className="footer__flex">
+    
+                <div className="footer__item">
+                    <p className="footer__item-title"><strong>For Employee</strong></p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid blanditiis culpa dolore ea
+                        eligendi, enim esse est et laboriosam, laborum magnam, numquam sunt suscipit unde voluptates. Ea
+                        quia repellat tempora!</p>
+                    <a href="" className="btn footer__btn">Go to Inventory</a>
                 </div>
-                
+    
+                <div className="footer__item">
+                    <p className="footer__item-title"><strong>Where to find us</strong></p>
+                    <div className="footer__item-address">
+                        <p>9 King Street</p>
+                        <p>London</p>
+                        <p>SE86 1NZ</p>
+                        <p>England</p>
+                        <p><strong>email:</strong> lorem@rsa.com </p>
+                        <p><strong>phone:</strong> (123) 123 123</p>
+                    </div>
+                </div>
+    
                 <div className="footer__social-buttons">
                     <a className="btn btn-primary" style={ {backgroundColor: '#3b5998'} } href="#" role="button">
                         <i className="fab fa-facebook-f social-icon"/>
                     </a>
-                    
+        
                     <a className="btn btn-primary" style={ {backgroundColor: '#55acee'} } href="#" role="button">
                         <i className="fab fa-twitter social-icon"/>
                     </a>
-                    
+        
                     <a className="btn btn-primary" style={ {backgroundColor: '#dd4b39'} } href="#" role="button">
                         <i className="fab fa-google social-icon"/>
                     </a>
-                    
+        
                     <a className="btn btn-primary" style={ {backgroundColor: '#ac2bac'} } href="#" role="button">
                         <i className="fab fa-instagram social-icon"/>
                     </a>
-                    
+        
                     <a className="btn btn-primary" style={ {backgroundColor: '#0082ca'} } href="#" role="button">
                         <i className="fab fa-linkedin-in social-icon"/>
                     </a>
                 </div>
             </div>
-        
+    
         </footer>
     );
 }
